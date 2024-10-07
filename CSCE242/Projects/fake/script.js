767693ea3f4b01526cdb50e1d58dacda3b3bf1e0
Nazire Brown <nlb1@email.sc.edu>
2024-10-06 20:13:12 -0400

    classes

--- CSCE242/Projects/fake/script.js
+++ CSCE242/Projects/fake/script.js
@@ -1,12 +1,7 @@
-document.getElementById("signup").addEventListener("click", () => {
-
-    alert("Membership Signup Successfull!");
-});
-
 const toggleNav = () => {
     document.getElementById("main-nav-items").classList.toggle("hidden");
 };
 
 window.onload = () => {
     document.getElementById("nav-toggle").onClick = toggleNav;
-}
+};
