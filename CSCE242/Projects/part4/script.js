4275676432aea6ef0f37604f894827971639e983
Nazire Brown <143841560+Nazireb@users.noreply.github.com>
2025-08-18 23:40:14 -0400

    Delete CSCE242 directory

--- CSCE242/Projects/part4/script.js
+++
@@ -1,7 +0,0 @@
-const nav = document.getElementById("nav");
-const navB = document.getElementById("smallnav");
-
-const smallnav = () => {
-    nav.classList.toggle("active");
-}
-navB.onclick = smallnav;
