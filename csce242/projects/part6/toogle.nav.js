e17a0f9aa720003a9c1d3687635281be2bfcf626
Nazire Brown <nlb1@email.sc.edu>
2025-10-08 22:51:31 -0400

    part6

--- csce242/projects/part6/toogle.nav.js
+++ csce242/projects/part6/toogle.nav.js
@@ -3,13 +3,40 @@ document.getElementById("toggle-nav").onclick = () => {
 }
 
 const getTravel = async() => {
-    const url = "";
+    const url = "https://nazireb.github.io/csce242/projects/part6/json/destinations.json";
 
     try {
         const response = await fetch(url);
         return response.json();
     }
     catch(error){
-        console.log("sorry");
+        console.log(error);
     }
 };
+
+const showTravel = async () => {
+    const travel = await getTravel();
+    const listDiv = document.getElementById("destinations-list");
+
+    travel.forEach((dest => {
+        const section = document.createElement("section");
+        section.classList.add("card");
+
+        const h3 = document.createElement("h3");
+        h3.innerHTML = dest.name;
+        section.append(h3);
+
+        const img = document.createElement("img");
+        img.src = dest.img_name;
+        section.append(img);
+
+        const p = document.createElement("p");
+        p.textContent = dest["short-desc"];
+        section.append(p);
+
+        listDiv.append(section);
+    }))
+
+}
+
+showTravel();
