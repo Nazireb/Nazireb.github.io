956eee2fd021372695b7e1a5c8412884438b9f3e
Nazire Brown <nlb1@email.sc.edu>
2025-10-08 21:24:24 -0400

    part6

--- csce242/projects/part6/toogle.nav.js
+++ csce242/projects/part6/toogle.nav.js
@@ -1,3 +1,15 @@
 document.getElementById("toggle-nav").onclick = () => {
     document.getElementById("nav-items").classList.toggle("hide-small");
 }
+
+const getTravel = async() => {
+    const url = "";
+
+    try {
+        const response = await fetch(url);
+        return response.json();
+    }
+    catch(error){
+        console.log("sorry");
+    }
+};
